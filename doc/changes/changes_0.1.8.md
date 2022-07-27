bbd6c53f70a95f2165bbd3f9ed458cb12cfbd0d1
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-07-27 10:46:55 +0200

    #19: Add context object

---
+++ doc/changes/changes_0.1.8.md
@@ -0,0 +1,7 @@
+# Extension Manager Interface 0.1.8, released 2022-??-??
+
+Code name: Add context type
+
+## Features
+
+* #19: Added context type
---
+++ src/context.ts
@@ -0,0 +1,41 @@
+
+/**
+ * This interface represents the context of the extension manager with useful information for the extensions.
+ */
+export interface Context {
+    /**
+     * The name of the schema in which extensions should be installed.
+     */
+    extensionSchemaName: string
+
+    /**
+     * An SQL client for executing queries and statements.
+     */
+    sqlClient: SqlClient
+
+    /**
+     * An object providing BucketFS related methods.
+     */
+    bucketFs: BucketFs
+}
+
+export interface BucketFs {
+    /**
+     * Resolves the given filename to an absolute path in BucketFS that is accessible by UDFs.
+     * 
+     * @param fileName the filename to resolve
+     * @returns the file's absolute path in BucketFS
+     */
+    resolvePath: (fileName: string) => string
+}
+
+/**
+ * Simple SQL client.
+ */
+export interface SqlClient {
+    /**
+     * Run a SQL query.
+     * @param query sql query string
+     */
+    runQuery: (query: string) => void
+}
