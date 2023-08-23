2fdc12fd2fc66f5f16055da9a71a82cde6c5739f
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-08-23 16:53:31 +0200

    Convert jest config to js

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,24 @@
-import type { Config } from 'jest';
 
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
-const config: Config = {
+const config = {
     clearMocks: true,
     collectCoverage: true,
     coverageDirectory: "coverage",
     coverageProvider: "v8",
+    errorOnDeprecated: true,
     preset: 'ts-jest',
+    testEnvironment: 'node',
     injectGlobals: false,
     modulePathIgnorePatterns: ['<rootDir>/dist/'],
+    testMatch: [
+        "**/?(*.)+(spec|test).ts"
+    ],
+    transform: {
+        "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }]
+    },
 };
 
 export default config;
