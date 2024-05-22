827ae2af12b052161b0febc7b52dd83d24719bee
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-05-22 09:17:29 +0200

    Fix sonar warning

--- src/base-vs-test/vsTestBase.ts
+++ src/base-vs-test/vsTestBase.ts
@@ -11,29 +11,27 @@ import { createMockContext } from '../base/test-utils';
  */
 export function testJavaVirtualSchemaBaseExtension(extensionProvider: () => ExasolExtension) {
     const extension = extensionProvider();
-    describe(`extension ${extension.name}`, () => {
-        describe("versions", () => {
-            it("has at least one version", () => {
-                expect(extension.installableVersions.length).toBeGreaterThan(0);
-            });
+    describe(`extension ${extension.name} versions`, () => {
+        it("has at least one version", () => {
+            expect(extension.installableVersions.length).toBeGreaterThan(0);
         });
-        describe("parameters", () => {
-            const parameters = extension.getInstanceParameters(createMockContext(), extension.installableVersions[0].name)
-            it("has at least one parameter", () => {
-                expect(parameters.length).toBeGreaterThan(0);
-            });
-            it("contains virtual schema name", () => {
-                expect(parameters.some(p => p.id === "baseVirtualSchemaName")).toBe(true);
-            });
-            it("have a unique ID", () => {
-                const ids = parameters.map(p => p.id);
-                expect(new Set(ids).size).toBe(ids.length);
-            });
-            for (const param of parameters) {
-                it(`parameter '${param.id}' conforms to naming conventions`, () => {
-                    expect(param.id).not.toContain(".");
-                });
-            }
+    });
+    describe(`extension ${extension.name} parameters`, () => {
+        const parameters = extension.getInstanceParameters(createMockContext(), extension.installableVersions[0].name)
+        it("has at least one parameter", () => {
+            expect(parameters.length).toBeGreaterThan(0);
         });
+        it("contains virtual schema name", () => {
+            expect(parameters.some(p => p.id === "baseVirtualSchemaName")).toBe(true);
+        });
+        it("have a unique ID", () => {
+            const ids = parameters.map(p => p.id);
+            expect(new Set(ids).size).toBe(ids.length);
+        });
+        for (const param of parameters) {
+            it(`parameter '${param.id}' conforms to naming conventions`, () => {
+                expect(param.id).not.toContain(".");
+            });
+        }
     });
 }
